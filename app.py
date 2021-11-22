f0e63507e867389413163a788867f00f0d0ed24b
Fabio Copa Mejia <64265042+fabi-cm@users.noreply.github.com>
2021-11-22 08:48:08 -0400

    new_index

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def inicio():
-    return "<h1>Hola mundo</h1>"
+def index():
+    return render_template('index.html')
+
+@app.route("/account")
+def account():
+    return render_template('signIn.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
