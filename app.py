dd2bd9191f9d6f4557d12a64c6a9bed24aea802c
Fabio Copa Mejia <64265042+fabi-cm@users.noreply.github.com>
2021-12-01 05:48:27 -0400

    db

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, Response
+from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
+from bson import json_utils
+from bson.objectId import ObjectId
+
+
 
 app = Flask(__name__)
+app.config['MONGO_URI']='mongodb://localhost/BD_netflix'
+mongo = PyMongo(app)
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/users', methods=['POST'])
+def crear_usuario():
+    # received Dato
+    print(request.json)
+    
+    return {'message': 'received'}
 
 @app.route("/account")
 def account():
